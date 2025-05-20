f70bc8944dbb7fd83eb342058295752946cc6522
JohnnyT <ubergeek3141@gmail.com>
2025-05-20 06:36:27 -0600

    Fixes typo in description

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Msg.MixProject do
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      description: "Micorosft Graph for Elixir",
+      description: "Microsoft Graph for Elixir",
       package: [
         licenses: ["MIT"],
         links: %{"GitHub" => "https://github.com/riddler/msg"}
