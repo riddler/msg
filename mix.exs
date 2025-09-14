28a8a011209a746ab5121148951911dbd3d6cfee
JohnnyT <ubergeek3141@gmail.com>
2025-09-14 07:03:26 -0600

    Releases v0.2.0 (#4)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Msg.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.2.0"
   @deps [
     # Docs - separated out to speed up dev compilcation
     {:ex_doc, "~> 0.31", only: :docs, runtime: false},
