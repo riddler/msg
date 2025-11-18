8bab5300fe7486041a772ac3d450f406f4014908
JohnnyT <ubergeek3141@gmail.com>
2025-11-18 06:16:35 -0700

    Releases v0.3.7 (#23)

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Msg.MixProject do
   use Mix.Project
 
   @app :msg
-  @version "0.3.6"
+  @version "0.3.7"
   @source_url "https://github.com/riddler/msg"
   @deps [
     # Docs - separated out to speed up dev compilcation
